b5d6d12432f48534860389d8514b240005658770
wei le <WEILE25.24@ichat.sp.edu.sg>
2025-05-07 23:07:30 +0800

    adding the function

--- LAB2.py
+++ LAB2.py
@@ -1,3 +1,10 @@
+def main():
+ calculate_bmi(height=1.73, weight=37)
+ display_main_menu()
+ F = get_user_input()
+ calc_average(F)
+ find_min_max(F)
+ pass
 def calculate_bmi(height, weight):
  print("Height = " + str(height))
  print("Weight = " + str(weight))
@@ -10,4 +17,28 @@ def calculate_bmi(height, weight):
  elif bmi>25:
   print("overweight")
   
-calculate_bmi(weight=37, height=1.73)
+
+def display_main_menu():
+ print("Enter some numbers seperated by commas (e.g. 5, 67, 32)")
+def get_user_input()-> list[float]:
+ x = input()
+ P = x.split(",")
+ mylist=P
+ F = [float(x) for x in mylist ]
+ print(F)
+ return(F)
+def calc_average(F):
+ a=(len(F))
+ b=(sum(F))
+ avg= b/a 
+ print("the avg temp is: " + str(avg))
+def find_min_max(F):
+ print("Min and Max: " + min(F)+max(F))
+ return [min(F), max(F)]
+def sort_temperature(F):
+ F.sort()
+ G=F[:]
+ return G
+
+if __name__ == "__main__":
+    main()
