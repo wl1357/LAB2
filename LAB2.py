76a604be331a388cb03299a3a7baa759e58d1579
wei le <WEILE25.24@ichat.sp.edu.sg>
2025-05-07 14:31:09 +0800

    comparing bmi

--- LAB2.py
+++ LAB2.py
@@ -1,1 +1,13 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight):
+ print("Height = " + str(height))
+ print("Weight = " + str(weight))
+ bmi= (weight)/(height)**2
+ print(bmi)
+ if bmi<18.5:
+  print("underweight")
+ elif bmi>=18.5 or bmi<=25:
+  print("normalweight")
+ elif bmi>25:
+  print("overweight")
+  
+calculate_bmi(weight=37, height=1.73)
